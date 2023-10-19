26e26533b96285631a80642b742b587a015e0bd5
SURESHROKAYA <sureshrokaya761@gmail.com>
2023-10-19 21:10:11 +0545

    Change

--- src/Home.jsx
+++ src/Home.jsx
@@ -56,6 +56,7 @@ export default function Home() {
                 <h3>We are the leading Luxury Products In over World</h3>
                 <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumed ut perspiciatis unde omnis iste natus error sit voluptatem accusaium doloremque laudantium.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis pariatur nemo, debitis facere enim odio quos incidunt ut exercitationem laboriosam natus, consectetur reiciendis molestiae aliquid eaque nostrum consequatur dolore sint?</p>
             </div>
         </div>
     </div>
